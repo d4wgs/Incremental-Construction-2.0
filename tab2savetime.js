6a3b9c9e33bc2ff6339864015d650bdbae838bc8
Steve K <68475815+d4wgs@users.noreply.github.com>
2022-04-27 18:22:07 -0400

    editing and fixing load function

--- tab2savetime.js
+++ tab2savetime.js
@@ -28,81 +28,12 @@ function openCity(evt, cityName) {
 
 function loadGame() {
   var savedGame = JSON.parse(localStorage.getItem("gameSave"));
-  if (typeof savedGame.Cash !== "udnefined") score = savedGame.Cash;
+  if (typeof savedGame.Cash !== "undefined") score = savedGame.Cash;
 }
 
 function saveGame() {
   var gameSave = {
     Cash: Cash,
-
-    ShackCost: 50,
-     Shack: 0,
-     ShackTot: 0,
-     ShackTotCost1: 50,
-
-     ApartmentCost: 300,
-     Apartment: 0,
-     ApartmentTot: 0,
-     ApartmentTotCost1: 300,
-
-     HouseCost: 1000,
-     House: 0,
-     HouseTot: 0,
-     HouseTotCost1: 1000,
-
-     MotelCost: 10000,
-     Motel: 0,
-     MotelTot: 0,
-     MotelTotCost1: 10000,
-
-     ApartmentComplexCost: 50000,
-     ApartmentComplex: 0,
-     ApartmentComplexTot: 0,
-     ApartmentComplexTotCost1: 50000,
-
-     TowerCost: 300000,
-     Tower: 0,
-     TowerTot: 0,
-     TowerTotCost1: 300000,
-
-     SkyscraperCost: 1000000,
-     Skyscraper: 0,
-     SkyscraperTot: 0,
-     SkyscraperTotCost1: 1000000,
-
-     Reputation: 100000,
-     ReputationCost: 3,
-     ReputationTot: 100000,
-
-     GDRepCost: 3,
-     GDRep: 0,
-     GDRepTot: 0,
-     GDRepTotCost1: 3,
-
-     VSRepCost: 10,
-     VSRep: 0,
-     VSRepTot: 0,
-     VSRepTotCost1: 10,
-
-     OTRepCost: 25,
-     OTRep: 0,
-     OTRepTot: 0,
-     OTRepTotCost1: 25,
-
-     ETRepCost: 60,
-     ETRep: 0,
-     ETRepTot: 0,
-     ETRepTotCost1: 60,
-
-     HCRepCost: 125,
-     HCRep: 0,
-     HCRepTot: 0,
-     HCRepTotCost1: 125,
-
-     MSARepCost: 1000,
-     MSARep: 0,
-     MSARepTot: 0,
-     MSARepTotCost1: 100
   };
   localStorage.setItem("gameSave", JSON.stringify(gameSave));
 }
