21d2cc939cd99ea7b160c118420d555d1c392e86
Steve K <68475815+d4wgs@users.noreply.github.com>
2022-04-27 15:49:37 -0400

    edits to upgrade functions
    Increased cost multiplier on all functions (1.1x -> 3.5x) and added the cost & output of building types to this sheet.

--- UpgradeFunctions.js
+++ UpgradeFunctions.js
@@ -2,20 +2,19 @@ function upgradeShack() {
     if (Cash >= ShackUpgradeCost) {
       Cash = Cash - ShackUpgradeCost;
       ShackOut = Math.round(ShackOut * 1.5);
-      ShackUpgradeCost = Math.round(ShackUpgradeCost * 1.1);
-      ShackCPS = ShackTot * ShackOut;
+      ShackUpgradeCost = Math.round(ShackUpgradeCost * 3.5);
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("ShackOut").innerHTML = ShackOut;
       document.getElementById("ShackUpgradeCost").innerHTML = ShackUpgradeCost;
-      document.getElementById("ShackCPS").innerHTML = ShackCPS;
+      document.getElementById("ShackUpgradesTot").innerHTML = ShackUpgradeTot;
     }
   }
 function upgradeApartment() {
     if (Cash >= ApartmentUpgradeCost) {
       Cash = Cash - ApartmentUpgradeCost;
       ApartmentOut = Math.round(ApartmentOut * 1.5);
-      ApartmentUpgradeCost = Math.round(ApartmentUpgradeCost * 1.1);
+      ApartmentUpgradeCost = Math.round(ApartmentUpgradeCost * 3.5);
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("ApartmentOut").innerHTML = ApartmentOut;
@@ -26,7 +25,7 @@ function upgradeHouse() {
     if (Cash >= HouseUpgradeCost) {
       Cash = Cash - HouseUpgradeCost;
       HouseOut = Math.round(HouseOut * 1.5);
-      HouseUpgradeCost = Math.round(HouseUpgradeCost * 1.1);
+      HouseUpgradeCost = Math.round(HouseUpgradeCost * 3.5);
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("HouseOut").innerHTML = HouseOut;
@@ -37,7 +36,7 @@ function upgradeMotel() {
     if (Cash >= MotelUpgradeCost) {
       Cash = Cash - MotelUpgradeCost;
       MotelOut = Math.round(MotelOut * 1.5);
-      MotelUpgradeCost = Math.round(MotelUpgradeCost * 1.1);
+      MotelUpgradeCost = Math.round(MotelUpgradeCost * 3.5);
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("MotelOut").innerHTML = MotelOut;
@@ -48,7 +47,7 @@ function upgradeApartmentComplex() {
     if (Cash >= ApartmentComplexUpgradeCost) {
       Cash = Cash - ApartmentComplexUpgradeCost;
       ApartmentComplexOut = Math.round(ApartmentComplexOut * 1.5);
-      ApartmentComplexUpgradeCost = Math.round(ApartmentComplexUpgradeCost * 1.1);
+      ApartmentComplexUpgradeCost = Math.round(ApartmentComplexUpgradeCost * 3.5);
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("ApartmentComplexOut").innerHTML = ApartmentComplexOut;
@@ -59,7 +58,7 @@ function upgradeTower() {
     if (Cash >= TowerUpgradeCost) {
       Cash = Cash - TowerUpgradeCost;
       TowerOut = Math.round(TowerOut * 1.5);
-      TowerUpgradeCost = Math.round(TowerUpgradeCost * 1.1);
+      TowerUpgradeCost = Math.round(TowerUpgradeCost * 3.5);
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("TowerOut").innerHTML = TowerOut;
@@ -70,10 +69,31 @@ function upgradeSkyscraper() {
     if (Cash >= SkyscraperUpgradeCost) {
       Cash = Cash - SkyscraperUpgradeCost;
       SkyscraperOut = Math.round(SkyscraperOut * 1.5);
-      SkyscraperUpgradeCost = Math.round(SkyscraperUpgradeCost * 1.1);
+      SkyscraperUpgradeCost = Math.round(SkyscraperUpgradeCost * 3.5);
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("SkyscraperOut").innerHTML = SkyscraperOut;
       document.getElementById("SkyscraperUpgradeCost").innerHTML = SkyscraperUpgradeCost;
     }
   }
+
+var ShackOut = 1;
+var ShackUpgradeCost = 1000;
+
+var ApartmentOut = 3;
+var ApartmentUpgradeCost = 6000;
+
+var HouseOut = 7;
+var HouseUpgradeCost = 20000;
+
+var MotelOut = 10;
+var MotelUpgradeCost = 200000;
+
+var ApartmentComplexOut = 12;
+var ApartmentComplexUpgradeCost = 1000000;
+
+var TowerOut = 15;
+var TowerUpgradeCost = 6000000;
+
+var SkyscraperOut = 25;
+var SkyscraperUpgradeCost = 20000000;
