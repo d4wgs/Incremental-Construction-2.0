1ba56448b409ec7e6287167215a61d2c0c57ffb6
Steve K <68475815+d4wgs@users.noreply.github.com>
2022-04-28 14:14:38 -0400

    removing extra colors

--- tab2savetime.js
+++ tab2savetime.js
@@ -337,16 +337,3 @@ function resetGame() {
     location.reload();
   }
 }
-
-var colors = ["#ff0000"
-, "#ee82ee"]
-var currentColor = 0
-var lis = document.querySelectorAll("#stepsId li")
-function changeColor() {
-  --currentColor
-  if (currentColor < 0) currentColor = colors.length -1
-  for (var i = 0; i < lis.length; i++) {
-    lis[i].style.color = colors[(currentColor +i) % colors.length]
-  }
-}
-setInterval(changeColor, 1000)
