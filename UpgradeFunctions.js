0d77c06e85b8e60549c5f864b9eb83ed1df6603a
Steve K <68475815+d4wgs@users.noreply.github.com>
2022-05-01 22:50:20 -0400

    Update UpgradeFunctions.js

--- UpgradeFunctions.js
+++ UpgradeFunctions.js
@@ -2,9 +2,9 @@ function upgradeShack() {
     if (Cash >= ShackUpgradeCost) {
       Cash = Cash - ShackUpgradeCost;
       ShackOut = Math.round(ShackOut * 1.5);
-      ShackOut = ShackOut;
       ShackUpgradeCost = Math.round(ShackUpgradeCost * 3.5);
       ShackUpgradeCost = ShackUpgradeCost;
+      ShackOut = ShackOut;
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("ShackOut").innerHTML = ShackOut;
@@ -16,6 +16,8 @@ function upgradeApartment() {
       Cash = Cash - ApartmentUpgradeCost;
       ApartmentOut = Math.round(ApartmentOut * 1.5);
       ApartmentUpgradeCost = Math.round(ApartmentUpgradeCost * 3.5);
+      ApartmentUpgradeCost = ApartmentUpgradeCost;
+      ApartmentOut = ApartmentOut;
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("ApartmentOut").innerHTML = ApartmentOut;
@@ -27,6 +29,8 @@ function upgradeHouse() {
       Cash = Cash - HouseUpgradeCost;
       HouseOut = Math.round(HouseOut * 1.5);
       HouseUpgradeCost = Math.round(HouseUpgradeCost * 3.5);
+      HouseUpgradeCost = HouseUpgradeCost;
+      HouseOut = HouseOut;
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("HouseOut").innerHTML = HouseOut;
@@ -38,6 +42,8 @@ function upgradeMotel() {
       Cash = Cash - MotelUpgradeCost;
       MotelOut = Math.round(MotelOut * 1.5);
       MotelUpgradeCost = Math.round(MotelUpgradeCost * 3.5);
+      MotelUpgradeCost = MotelUpgradeCost;
+      MotelOut = MotelOut;
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("MotelOut").innerHTML = MotelOut;
@@ -49,6 +55,8 @@ function upgradeApartmentComplex() {
       Cash = Cash - ApartmentComplexUpgradeCost;
       ApartmentComplexOut = Math.round(ApartmentComplexOut * 1.5);
       ApartmentComplexUpgradeCost = Math.round(ApartmentComplexUpgradeCost * 3.5);
+      ApartmentComplexOut = ApartmentComplexOut;
+      ApartmentOut = ApartmentOut;
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("ApartmentComplexOut").innerHTML = ApartmentComplexOut;
@@ -60,6 +68,8 @@ function upgradeTower() {
       Cash = Cash - TowerUpgradeCost;
       TowerOut = Math.round(TowerOut * 1.5);
       TowerUpgradeCost = Math.round(TowerUpgradeCost * 3.5);
+      TowerUpgradeCost = TowerUpgradeCost;
+      TowerOut = TowerOut;
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("TowerOut").innerHTML = TowerOut;
@@ -71,6 +81,8 @@ function upgradeSkyscraper() {
       Cash = Cash - SkyscraperUpgradeCost;
       SkyscraperOut = Math.round(SkyscraperOut * 1.5);
       SkyscraperUpgradeCost = Math.round(SkyscraperUpgradeCost * 3.5);
+      SkyscraperUpgradeCost = SkyscraperUpgradeCost;
+      SkyscraperOut = SkyscraperOut;
 
       document.getElementById("Cash").innerHTML = Cash;
       document.getElementById("SkyscraperOut").innerHTML = SkyscraperOut;
