f8d2a6072eab13b35c69b8908e0f15efab310e09
Steve K <68475815+d4wgs@users.noreply.github.com>
2022-05-06 20:18:04 -0400

    Update hiddens.js

--- hiddens.js
+++ hiddens.js
@@ -1,4 +1,4 @@
-if (Cash >= 130) {
+if (Shack >= 1) {
     document.getElementById("Bricks_progress").style.visibility = "visible";
     document.getElementById("Bricks_bar").style.visibility = "visible";
 }
