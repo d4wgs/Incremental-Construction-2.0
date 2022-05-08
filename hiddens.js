994e122bfc4e9b5e651d7720a12525bd3ded97dc
Steve K <68475815+d4wgs@users.noreply.github.com>
2022-05-07 22:07:15 -0400

    changing bricks bar requirement

--- hiddens.js
+++ hiddens.js
@@ -1,4 +1,4 @@
-if (Shack >= 1) {
+if (Shack >= 5) {
     document.getElementById("Bricks_progress").style.visibility = "visible";
     document.getElementById("Bricks_bar").style.visibility = "visible";
 }
